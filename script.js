6ad1d487772c62dc07999fc94d0c7ec9941a704c
Victor Taiwo <51122045+Smallz97@users.noreply.github.com>
2023-09-08 23:44:16 +0100

    completed styling for mobile navigation, included comments in project build files

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// write your JavaScript here
+// Setting variables ...
+const mobileMenu = document.getElementById("mobile-menu");
+
+// The function to close the mobile navigation menu
+const openMobileMenu = () => {
+    mobileMenu.style.width = "100vw";
+}
+
+// The function to close the mobile navigation menu
+const closeMobileMenu = () => {
+    mobileMenu.style.width = "0";
+}
